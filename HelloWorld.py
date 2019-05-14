411bc6b266b407410ed3830a88e32adfae6203cf
Marcelloos <oosthuizenmarcell@gmail.com>
2019-05-14 22:50:14 +0200

    some_message

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,11 @@
-a = "Hello World"
+class world:
 
-for i in range(14):
-	if(i%2 == 0):
-		print(a)
-	else:
-		print("not so hello!!!!")
+	def callMe(self):
+		for i in range(14):
+			if(i%2 == 0):
+				print("Hello World !!!")
+			else:
+				print("not so hello !!!!")
+
+b = world()
+b.callMe()
